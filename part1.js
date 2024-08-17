91f6048507606d601bf4d8e9f934280a5d6fa480
Roman Kutselepa <roman.kutselepa@gmail.com>
2024-08-17 14:28:14 +0300

    first commit

---
+++ part1.js
@@ -0,0 +1,91 @@
+// let a;  // "undefined"
+// a = 'Roman';
+// console.log('Hello', a);
+
+// let b = 10, c = 5;
+// console.log(b + c * 2);
+
+// a = '123';
+// console.log('Hello', a);
+
+// const d = 'Hello';
+
+// var x;
+// x = 10;
+// console.log(x);
+
+// var x = 20;
+// console.log(x);
+
+// let x = 122;
+// console.log(typeof x);
+// console.log(x);
+
+// // console.log((0.1 * 10 + 0.2 * 10) / 10);
+
+// // console.log((10 * 5) ** 2);
+
+// console.log(9 % 4);  // 9 = 4 + 4 + 1
+// console.log(9 / 4);
+// console.log(++x);
+// console.log(x);
+
+// console.log(typeof NaN);
+
+// let userName = "\tTerminator\nasdasda";
+
+// console.log(typeof userName);
+// console.log(userName.length);
+// console.log(userName.toUpperCase());
+
+// console.log('T\\hat\'s'.length);
+// console.log(userName);
+
+
+// let isPopular = true;
+
+// console.log(null);
+
+// let a = null;
+// let b;
+
+// let num = NaN;  // 0, -0, NaN --> false
+// console.log(Boolean(num));
+
+// let str = '';  // empty string --> false
+// console.log(Boolean(str));
+
+
+// let a = 10;
+
+// // console.log(A);
+
+// console.log('a' > 'A');
+
+// let car = 'Subaru WRX';
+//         // 0123456789
+// console.log(car.length);
+// console.log(car[car.length - 1]);
+
+// let make = car.slice(0, 6);
+// console.log(car[4]);
+// console.log(car.endsWith('u WRX'));
+// console.log(car.includes('aru WRX', 3));
+// console.log(car.indexOf('WRX', 1));
+
+// let x = 'Hello';
+// console.log(x.padEnd(10, '*'));
+// console.log('*'.repeat(10));
+// console.log(x.replaceAll('l', ''));
+
+// let ger = 'der Fluß';
+
+// console.log(ger.toLocaleLowerCase());
+// console.log(ger.toLowerCase());
+// // console.log(ger.sub);
+
+
+let userName = '    Jack Smith  ';
+console.log(userName.length);
+// console.log(userName.trim);
+
